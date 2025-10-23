809bcdd3de9fbdcb81f981b4f122af58b6935181
jancistas <jan.stas105@gmail.com>
2025-10-23 14:25:54 +0200

    Fix: Remove unused Link import from ProductDetail

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { products } from '../data/products';
 import { Product } from '../types';
 import { formatPrice, calculateDiscount, formatDiscountPercentage } from '../utils/price';
